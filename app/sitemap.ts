537b3fa4581d8d10899d6b1684b7fb0ea4a07541
OMAR <rockstonestudios666@gmail.com>
2026-05-22 11:41:17 -0500

    sitemaps dinamico ..

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,25 +1,3 @@
-// import { MetadataRoute } from 'next';
-
-// // 💡 Eliminamos ": MetadataRoute.Sitemap" del nombre de la función para que TypeScript infiera el tipo automáticamente sin chocar
-// export default async function sitemap() {
-//   const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "https://oasis-directorio-ccg7.vercel.app";
-
-//   const routes = [
-//     '',               
-//     '/mapa',          
-//     '/business',      
-//     '/categorias',    
-//     '/category',      
-//     '/municipality',  
-//   ].map((route) => ({
-//     url: `${baseUrl}${route}`,
-//     lastModified: new Date(), // Usar un objeto Date nativo es más seguro para el tipo Sitemap de Next.js
-//     changeFrequency: 'daily' as const,
-//     priority: route === '' ? 1.0 : 0.8,
-//   }));
-
-//   return routes;
-// }
 
 import { MetadataRoute } from 'next';
 // 💡 Importa tu cliente de Sanity o la función con la que traes tus datos
