2a84405e48d4004623d0d22600e8645d5842a7d5
OMAR <rockstonestudios666@gmail.com>
2026-07-18 21:10:43 -0500

    Zoom 9

--- app/incendios/page.tsx
+++ app/incendios/page.tsx
@@ -133,7 +133,7 @@ function IncendiosMapViewComponent() {
         container: mapContainerRef.current,
         style: mapStyle,
         center: ANTIOQUIA_CENTER,
-        zoom: 8.6,
+        zoom: 9.1,
         pitch: 35,  
         bearing: 0,
         antialias: true
